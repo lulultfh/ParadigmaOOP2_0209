639f3747f06b02d1d1da494cf768cb8a67ebb754
lulultfh <punyalulu05@gmail.com>
2024-05-28 15:10:07 +0700

    tadi salah ini utk virtual funtionnya (maaf ya mas🙏)

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 class seseorang {
 public:
-    // virtual void pesan() = 0;
+    virtual void pesan() = 0;
     // virtual void pesan(){
     //     cout << "pesan dari seseorang" << endl;
     // }
 };
+
