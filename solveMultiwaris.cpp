019544018f46b551eab757c1fde890f2d0bc245e
lulultfh <punyalulu05@gmail.com>
2024-05-28 15:01:40 +0700

    add class budi from class pekerja and class pelajar

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -26,3 +26,10 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) : pekerja (pUmur), pelajar(pUmur) {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
