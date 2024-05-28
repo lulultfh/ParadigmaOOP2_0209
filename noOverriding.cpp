cb8103a29469cb1cf314eccbdc4117ede464e3cf
lulultfh <punyalulu05@gmail.com>
2024-05-28 15:40:24 +0700

    copy paste from project noPewarian

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan() {
+        cout << "Halo! Saya function dari Base Class";
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << " Halo! Saya function dari Derived class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
